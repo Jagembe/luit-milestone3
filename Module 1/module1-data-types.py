6cb9c073b07ccad02ee5980aa12d5741c50a517e
CODE-NXTDOOR <jsantoine24@gmail.com>
2024-08-13 18:33:27 -0500

    Create module1-data-types.py

---
+++ Module 1/module1-data-types.py
@@ -0,0 +1,15 @@
+# String
+greeting = "Hello, friend!"
+
+# Integer
+age = 24
+
+# Floats
+speed1 = 6.3
+speed2 = 9.2
+speed3 = 4.
+speed4 = 4
+
+# Booleans (values must be capitalized ex: True or False)
+# Note: When dealing with  booleans 1=True and 0=False
+am_i_ugly = True
