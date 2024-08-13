3c6c389cee09319e328d5556f3234317e46432e1
CODE-NXTDOOR <jsantoine24@gmail.com>
2024-08-13 18:26:37 -0500

    Update module 1-variables.py

--- module 1-variables.py
+++ module 1-variables.py
@@ -2,3 +2,14 @@ greeting = "Hello, friend!"
 print(greeting)
 new_greeting = "Cheers"
 print(new_greeting)
+
+# Note: When writing a variable it must start with either an '_' or a letter.
+# _correctName
+# correctName
+# correctName89
+# 89incorrect
+
+# Note: upper and lowercase letters are treated differently. greeting vs. Greeting are two different things
+
+# Note: Python also has a couple of key words that cannot be used as variable names like 'from' or 'global'
+
