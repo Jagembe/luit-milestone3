76f31640dc88ddec7128abdeb34e62e1e9b5d8f2
CODE-NXTDOOR <jsantoine24@gmail.com>
2024-08-13 18:57:52 -0500

    Create module1-numerical-representations.py

---
+++ module1-numerical-representations.py
@@ -0,0 +1,17 @@
+# underscores in numbers
+# they are both the same value but the second one is easier too read
+12000300
+12_000_300
+
+# scientific notation
+# 3e4 = 3E4 = 3 * 10000 = 30000
+# 3e-4 = 3E-4 = 3 * 1/10000 = 0.0003
+print(0.00000000000000000003)
+
+# octal numbers
+# start with 0O or 0o
+print(0o123)
+
+# hexadecimal numbers
+# start with 0X or 0x
+print(0x123)
