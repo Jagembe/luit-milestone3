66cdc7045d945cf1f781a92c2b5a8309e96cf328
Jagembe <58337007+Jagembe@users.noreply.github.com>
2024-10-24 23:17:37 -0400

    clean-up

--- Module 2/module2-more-about-operators.py
+++
@@ -1,3 +0,0 @@
-# Order of operators (PEMDAS)
-# Python knows the order of operations in (2 + 3) * 2 = 10
-
--- Module 4/module4-list-comprehenstions.py
+++
@@ -1,8 +0,0 @@
-# list comprehensions
-# what if you need to create a really long list?
-
-numbers = [i for i in range(1, 101)]
-print(numbers) # printing number from 1 to 100
-
-numbers = [i for i in range(1, 101) if i % 3 != 0] # excluding numbers that are divisible by 3 from the list different from 0
-print(numbers) 
